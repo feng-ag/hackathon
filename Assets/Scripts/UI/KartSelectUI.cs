544a84f7b58fa3d5345965e439ccfc6a26f6045f
Feng <fengchen31@gmail.com>
2022-04-23 11:37:12 +0800

    replace car model with cub model

--- Assets/Scripts/UI/KartSelectUI.cs
+++ Assets/Scripts/UI/KartSelectUI.cs
@@ -5,30 +5,33 @@ using UnityEngine.UI;
 
 public class KartSelectUI : MonoBehaviour
 {
-	public Image speedStatBar;
-	public Image accelStatBar;
-	public Image turnStatBar;
+    public Image speedStatBar;
+    public Image accelStatBar;
+    public Image turnStatBar;
 
-	private void OnEnable() {
-		SelectKart(ClientInfo.KartId);
-	}
+    private void OnEnable()
+    {
+        UnityEngine.Debug.Log(ClientInfo.KartId);
+        SelectKart(ClientInfo.KartId);
+    }
 
-	public void SelectKart(int kartIndex)
-	{
-		ClientInfo.KartId = kartIndex;
+    public void SelectKart(int kartIndex)
+    {
+        ClientInfo.KartId = kartIndex;
         if (SpotlightGroup.Search("Kart Display", out SpotlightGroup spotlight)) spotlight.FocusIndex(kartIndex);
-		ApplyStats();
+        ApplyStats();
 
-        if ( RoomPlayer.Local != null ) {
+        if (RoomPlayer.Local != null)
+        {
             RoomPlayer.Local.RPC_SetKartId(kartIndex);
         }
-	}
+    }
 
-	private void ApplyStats()
-	{
-		KartDefinition def = ResourceManager.Instance.kartDefinitions[ClientInfo.KartId];
-		speedStatBar.fillAmount = def.SpeedStat;
-		accelStatBar.fillAmount = def.AccelStat;
-		turnStatBar.fillAmount = def.TurnStat;
-	}
+    private void ApplyStats()
+    {
+        KartDefinition def = ResourceManager.Instance.kartDefinitions[ClientInfo.KartId];
+        speedStatBar.fillAmount = def.SpeedStat;
+        accelStatBar.fillAmount = def.AccelStat;
+        turnStatBar.fillAmount = def.TurnStat;
+    }
 }
