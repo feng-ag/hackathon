48f28c1541be913977df6d87f13dd9bf6c026e55
Chen-Xiang-Ji <a841003@gmail.com>
2022-04-24 13:27:02 +0800

    [CubEditor] 場景 CarEdit  從拖拉旋轉UI 模型
    [使用]
    打開 CarEdit  場景 play 後用滑鼠點按模型處拖拉即可選轉模型 Y 軸
    
    [細節]
    1. 拖拉判定區域為廠境內 UIRoot/HiddenMouseDraggingArea 物件下的 image 範圍
    將顏色調製可見即可 Debug
    2. 該物件下的 RotateModelPanel  即為控制腳本

---
+++ Assets/Scripts/CubEditor/UI/RotateModelPanel.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+namespace ArcadeGalaxyKit
+{
+    public class RotateModelPanel : MonoBehaviour
+         , IPointerDownHandler
+         , IDragHandler
+         , IPointerEnterHandler
+         , IPointerExitHandler
+         , IPointerUpHandler
+    {
+        bool isPointerEnter = false;
+        bool isPointerExit = false;
+        bool isPointerDragging = false;
+        float xRotateAmount = 0.0f;
+        Vector2 clickPosition;
+
+        [Header("UI需選轉的場景物件")]
+        public GameObject objToRotate;
+
+        /// <summary>
+        /// Rotate cub model by x degree
+        /// </summary>
+        public void SetRotateModelY(float xOffset)
+        {
+            if (objToRotate)
+            {
+                Vector3 temp = objToRotate.transform.localRotation.eulerAngles;
+                Quaternion quaternion = objToRotate.transform.localRotation;
+                temp.y = xOffset;
+                quaternion.eulerAngles = temp;
+                objToRotate.transform.localRotation = quaternion;
+            }
+        }
+        public void OnDrag(PointerEventData eventData)
+        {
+            if (isPointerEnter && isPointerDragging && !isPointerExit)
+            {
+                SetRotateModelY(clickPosition.x-eventData.position.x);
+                isPointerDragging = true;
+            }
+        }
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            isPointerEnter = true;
+            isPointerExit = false;
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            if (isPointerDragging) SetRotateModelY(clickPosition.x - eventData.position.x);
+            isPointerEnter = false;
+            isPointerExit = true;
+            
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            if (isPointerDragging) {
+                if (isPointerDragging) SetRotateModelY(clickPosition.x - eventData.position.x);
+                isPointerDragging = false;
+            }
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            isPointerDragging = true;
+            clickPosition = eventData.position;
+        }
+    }
+}
