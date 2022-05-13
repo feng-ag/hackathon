fed39715622ec4a811546c2a3a79bf851cf31ed7
Chen-Xiang-Ji <a841003@gmail.com>
2022-05-13 10:58:35 +0800

    [NFT Generator]  因應依照系列限制生成 skin 種類，為 CubComponentSetting.cs 新增新標籤參數
    [細節]
    1. CubComponentSetting.cs  新增 ComponentSeries 標記該 Component 所屬系列
    2. ComponentSeries  為 ScriptableObject 方便後續資料可擴充

---
+++ Assets/Scripts/CubEditor/Data/ComponentSeries.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace ArcadeGalaxyKit
+{
+    [CreateAssetMenu(
+     menuName = "ArcadeGalaxyKit/CubEditor/ComponentSetting/ComponentSeries",
+     fileName = "ComponentSeries")]
+    [Serializable]
+    public class ComponentSeries : ScriptableObject
+    {
+        public string seriesName;
+    }
+}
--- Assets/Scripts/CubEditor/Data/ComponentSetting/CubComponentSetting.cs
+++ Assets/Scripts/CubEditor/Data/ComponentSetting/CubComponentSetting.cs
@@ -3,6 +3,31 @@ using UnityEngine;
 
 namespace ArcadeGalaxyKit
 {
+    // Setting {}
+    // AnimalSetting Preset : Tirger
+    // TraitType : Tirger
+    // SkinOption : SkinTiger1,SkinTiger2,SkinTiger2,SkinTiger3,SkinCommon          : CubComponentSetting
+
+
+    // Data Format
+    // Option1 : 在 Skin:Component 下加 string AnimalType 標記
+    //   優 : 資料面乾淨
+    //   缺 : Method 需要掃過一次所有 Skins
+    //   缺 : string AnimalType 分散在各個檔案沒辦法一次掌握
+    // Option2 : 另外製作專門 for AnimalType 類型的容器儲存所有相關的 Skin 選項
+    //   優 : Method 只需要拿到容器便可以 Random
+    //   缺 : Common Skin 需要另外加載，切無法歸類
+    // Conclusion : 採用 Option1 再另外產生 Option2 所需的資料
+
+    //Method ViewPoint
+    //流程 -> 選 "動物" (對應外部特徵)
+    //     -> 選 skin 
+    //       -> 決定可選 skin (根據 "動物" 類型 ) -> Data 
+    //       -> 選 skin
+    //     -> 選 輪子
+    //     -> 選 眼鏡
+    //     -> 選 眼睛 
+
     /// <summary>
     /// ComponentSetting Base
     /// </summary>
@@ -12,5 +37,6 @@ namespace ArcadeGalaxyKit
         public string styleName;
         public Sprite UIIcon;
         public int rate;
+        public ComponentSeries series;
     }
 }
